e1a58306c7fe479710a1a1dc389459ccaa748564
jhonatabezerra <jhonata.empresa+github@gmail.com>
2025-11-23 23:33:45 +0000

    Revert "Angular implementation"
    This reverts commit a942299faa15c7979b485b41f71e83ff038a7d11.

--- FinanceTracker/src/FinanceTracker.Web/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://your-api-url.com' // Ajuste conforme a URL da sua API em produção
-};
-
--- FinanceTracker/src/FinanceTracker.Web/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7000' // Ajuste conforme a URL da sua API
-};
-
